0b9b2a0c870f428a13e887d8ea4c63de4dbb9d81
Dylan Wilkinson <dylanwilkinson@gmail.com>
2017-08-07 17:08:08 -0700

    Added header/footer

--- wp-content/themes/simple/index.php
+++ wp-content/themes/simple/index.php
@@ -1,13 +1,23 @@
-<!DOCTYPE html>
-<html <?php language_attributes() ?>>
-<head>
-	<meta charset="<?php bloginfo('charset'); ?>"
-	<title><?php bloginfo('name') ?></title>
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-	<?php wp_head(); ?>
-</head>
+<?php get_header(); ?>
 
-<body>
+
+<div class="main">
+	<div class="container">
+		<?php if ( have_posts() ): ?>
+			<?php while ( have_posts() ): the_post(); ?>
+				<h3><?php the_title(); ?></h3>
+				<div class="meta">
+					Created By <?php the_author(); ?> on <?php the_time( 'F j, Y g:i a' ); ?>
+				</div>
+				<?php the_content(); ?>
+			<?php endwhile; ?>
+		<?php else: ?>
+			<?php echo wpautop( 'Sorry, No posts were found' ); ?>
+		<?php endif; ?>
+	</div>
+</div>
+
+<?php get_footer(); ?>
 
 </body>
 
